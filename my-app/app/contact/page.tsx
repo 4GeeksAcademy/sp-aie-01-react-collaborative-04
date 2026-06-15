4c02079b2b9c9f41a6f89037655aa0eccc23ad9f
thispedrito1 <thispedrito1@gmail.com>
2026-06-15 19:46:56 +0000

    thispedrito1form

--- my-app/app/contact/page.tsx
+++ my-app/app/contact/page.tsx
@@ -1,10 +1,9 @@
-import Navbar from "../components/Navbar";
-
-
 export default function ContactPage() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Contact Page</h1>
+      <h1 className="text-3xl font-bold text-zinc-900 dark:text-zinc-100">Contact Page
+      </h1>
+
     </div>
   );
 }
