9b7d1c34fe9f420711a3a78c976a3a3afaeb720e
Daniel <danielvf24@gmail.com>
2026-06-15 16:59:32 +0000

    Create the navbar and adding it to layout.tsx

---
+++ my-app/app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function Navbar() {
+
+    return (
+        <div>
+            <nav className="flex justify-between items-center p-4">
+                <ul className="flex gap-6">
+                    <li>
+                        <Link href="/" className="hover:underline">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/myFilms" className="hover:underline">
+                            My films
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className="hover:underline">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" className="hover:underline">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
