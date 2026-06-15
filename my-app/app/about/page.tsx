86a7051154c020b6c3612937ee679887a9e28c2f
Alberto Alvarez <betoalzu12@gmail.com>
2026-06-15 17:34:01 +0000

    Creacion de las paginas bases

---
+++ my-app/app/about/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+export default function ContactPage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Contact Page</h1>
+    </div>
+  );
+}
