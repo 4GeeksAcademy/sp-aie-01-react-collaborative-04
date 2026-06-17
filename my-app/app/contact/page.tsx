5e1c0bacd07539d2919f6ec5ad978c7402f903d1
Daniel <danielvf24@gmail.com>
2026-06-17 17:12:58 +0000

    Eliminar la página de contacto

--- my-app/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ContactPage() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-3xl font-bold text-zinc-900 dark:text-zinc-100">Contact Page
-      </h1>
-
-    </div>
-  );
-}
