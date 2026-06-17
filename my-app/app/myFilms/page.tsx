a0e7dbf4e7434f7a199416655c8466953ec89246
Daniel <danielvf24@gmail.com>
2026-06-17 18:15:07 +0000

    Agregar nuevas páginas para alberto, danifer24, itziar y thispedrito1; eliminar la página My Films

--- my-app/app/myFilms/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "../components/Navbar";
-
-export default function MyFilmsPage() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>My Films Page</h1>
-    </div>
-  );
-}
