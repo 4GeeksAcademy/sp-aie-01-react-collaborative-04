208d691a8ba7e8ae30bd3a4d2603abac9488d207
Daniel <danielvf24@gmail.com>
2026-06-17 16:03:19 +0000

    Contact dropdown menu

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -21,10 +21,34 @@ export default function Navbar() {
                             About
                         </Link>
                     </li>
-                    <li>
-                        <Link href="/contact" className="hover:underline">
-                            Contact
-                        </Link>
+                    <li className="relative">
+                        <details className="group">
+                            <summary className="cursor-pointer list-none hover:underline">
+                                Contact
+                            </summary>
+                            <ul className="absolute left-0 top-full mt-2 min-w-44 rounded-md border bg-black p-2 shadow-md">
+                                <li>
+                                    <Link href="/contact/thispedrito1" className="block rounded px-3 py-2 hover:bg-gray-100">
+                                        Pedro
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact/danifer24" className="block rounded px-3 py-2 hover:bg-gray-100">
+                                        Daniel
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact/alberto" className="block rounded px-3 py-2 hover:bg-gray-100">
+                                        Alberto
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/contact/itziar" className="block rounded px-3 py-2 hover:bg-gray-100">
+                                        Itziar
+                                    </Link>
+                                </li>
+                            </ul>
+                        </details>
                     </li>
                 </ul>
             </nav>
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-white">
       <h1>Página de inicio</h1>
     </div>
   );
